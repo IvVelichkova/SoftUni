2d2a5850f86f64a4016975fe6e68d1a588927e4f
IvVelichkova <night_princess.1986@abv.bg>
2017-06-20 14:38:05 +0300

    ReplaceAtag

---
+++ ProgramReplaceAtag.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReplaceAtag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(<a) (\w+=""http:\/\/softuni.bg"">SoftUni)(<\/a>)";
+            string replacement = " ";
+            while (input!="end")
+                
+            {
+                var regex = new Regex(pattern);
+                Console.WriteLine();
+
+
+
+                    
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
