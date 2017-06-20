cd6b025010cb62a80d6c355645466ad8fe755524
IvVelichkova <night_princess.1986@abv.bg>
2017-06-20 14:38:34 +0300

    MatchFullName

---
+++ ProgramMatchFullName.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string pattern = @"\b^[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+\b";
+            string userName = Console.ReadLine();
+
+
+            while (userName!="end")
+            {
+
+                var regex = new Regex(pattern);
+
+                if (regex.IsMatch(userName))
+                {
+                    Console.WriteLine(userName);
+                }
+
+                userName = Console.ReadLine();
+
+            }
+
+
+
+
+        }
+    }
+}
+
