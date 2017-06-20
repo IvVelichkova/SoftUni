4ec1bbf4b403aca3050f4f3ac05b340a669c429a
IvVelichkova <night_princess.1986@abv.bg>
2017-06-20 14:35:22 +0300

    ExtractEmails

---
+++ ProgramExtractEmails.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+
+            string pattern = @"^(([a-z]|[0-9])+([a-zA-Z0-9\d.\d_\d-]+)@(\w+[a-z.]+[a-z]))\$";
+
+            Regex regex = new Regex(@"\b(([a-z]|[0-9])([a-zA-Z0-9\d.\d_\d-]+)@(\w+[a-z.-]+[a-z]))\b");
+
+            MatchCollection matches = regex.Matches(input);
+            foreach (Match m in matches)
+            {
+                Console.WriteLine(m.Groups[1]);
+            }
+        }
+    }
+}
