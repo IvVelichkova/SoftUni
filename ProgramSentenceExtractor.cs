73a191e5454b7ac96912c9ab8519ab2124fa27c6
IvVelichkova <night_princess.1986@abv.bg>
2017-06-20 14:37:28 +0300

    Sentence Extractor

---
+++ ProgramSentenceExtractor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SentenceExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string constant = Console.ReadLine();
+            string text = Console.ReadLine();
+            string pattern= "[A-Z][\\w\\W]*?\b" + constant+
+            "\b[\\w\\W]*?[.!?]";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(text);
+
+            if (match.Success)
+            {
+                Console.WriteLine(match.Value);
+            }
+
+        }
+    }
+}
